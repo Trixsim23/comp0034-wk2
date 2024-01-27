22c1d0fd758683d684eb2128c6106513bec2b0dd
trixia <trixiasimangan@gmail.com>
2024-01-27 17:07:13 +0000

    changing secret key and changes to paralympics.py

--- paralympics/paralympics.py
+++ paralympics/paralympics.py
@@ -3,8 +3,13 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-
-@app.route('/')
-@app.route("/<name>")
+@app.route("/")
 def hello(name=None):
+    return f"Hello World!"
+
+@app.route("/Trixia")
+def hello_greeting(name='Trixia'):
     return f"Hello, {escape(name)}!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
